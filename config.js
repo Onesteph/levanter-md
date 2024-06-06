bcbfe5ba92fa917d01e40fb7caaf4f1349025043
Onesteph <the.vanessaharvey@gmail.com>
2024-06-06 07:18:49 +0100

    Update config.js
    Signed-off-by: Onesteph <the.vanessaharvey@gmail.com>

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '6_6_5619_458e_86d6').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
